9c39f2679d4b6da9a94266481143d40b7a54a078
Dhruvajyoti Sarma <onlinedhruva71@gmail.com>
2024-07-16 16:30:14 +0530

    Added Embedder class

--- components/base_classes.py
+++ components/base_classes.py
@@ -2,24 +2,45 @@ from abc import ABC, abstractmethod
 
 
 class Retriever(ABC):
+    """
+    Base class for Retriever. This provides the interface for retrieving documents.
+    """
     @abstractmethod
     def retrieve(self, query):
         pass
 
 
 class Reader(ABC):
+    """
+    Base class for Reader. This provides the interface for reading documents.
+    """
     @abstractmethod
     def read(self, documents):
         pass
 
 
 class Generator(ABC):
+    """
+    Base class for Generator. This provides the interface for generating text using a reader output.
+    """
     @abstractmethod
     def generate(self, reader_output):
         pass
 
 
+class Embedder(ABC):
+    """
+    Base class for Embedder. This provides the interface for embedding text.
+    """
+    @abstractmethod
+    def embed(self, text):
+        pass
+
+
 class RAGStrategy(ABC):
+    """
+    Base class for RAG strategy.
+    """
     @abstractmethod
     def retrieve(self, query):
         pass
@@ -30,4 +51,8 @@ class RAGStrategy(ABC):
 
     @abstractmethod
     def generate(self, reader_output):
+        pass
+
+    @abstractmethod
+    def embed(self, text):
         pass
