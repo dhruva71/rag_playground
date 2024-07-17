21e94e388ae0bfebb2097c7a0e3a971e2eae26ff
Dhruvajyoti Sarma <onlinedhruva71@gmail.com>
2024-07-17 14:35:02 +0530

    Added openai llm class

---
+++ components/llms/llm_openai.py
@@ -0,0 +1,22 @@
+import os
+from openai import OpenAI
+from components import base_classes
+
+
+class LLMOpenAI(base_classes.LLM):
+    def __init__(self, api_key: str, model: str = "gpt-3.5-turbo"):
+        self.api_key = api_key
+        self.client = OpenAI(api_key=api_key)
+        self.model = model
+
+    def generate(self, prompt: str):
+        chat_completion = self.client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt,
+                }
+            ],
+            model=self.model,
+        )
+        return chat_completion.choices[0].message.content
